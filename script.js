23df8fefb67a5e3226ddf0e14de92957d38f4592
Pritom Dey <pritomdey4@gmail.com>
2021-02-07 15:06:22 +0600

    final

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-const searchBtn=document.getElementById("search-btn");
+const searchBtn = document.getElementById("search-btn");
 
 
 ////////////////////////////////////////
 //Adding event handler on search button
 /////////////////////////////////////////
-searchBtn.addEventListener("click",() => {
-    let searchVal=document.getElementById("search-value").value;
-    searchVal=searchVal.trim();
-    if(searchVal!=""){
-        findMeals(searchVal);
-    }
-    else{
-        let msg="Please enter a meal name.";
-        displayAlert(msg,"red");
-    }
-    
+searchBtn.addEventListener("click", () => {
+	let searchVal = document.getElementById("search-value").value;
+	searchVal = searchVal.trim();
+	if (searchVal != "") {
+		findMeals(searchVal);
+	} else {
+		let msg = "Please enter a meal name.";
+		displayAlert(msg, "red");
+	}
+
 });
 
 
@@ -23,25 +22,24 @@ searchBtn.addEventListener("click",() => {
 //////////////////////////////////////////////////////
 
 const findMeals = (searchValue) => {
-    const mealList=document.getElementById("meal-list");
-    mealList.innerHTML="";
-   //Calling api by first letter of food name
+	const mealList = document.getElementById("meal-list");
+	mealList.innerHTML = "";
+	//Calling api by first letter of food name
 
-   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`).then((response) => {
-    return response.json();
-    }).then((mealData) => {    
-     const meals=mealData.meals;
-   
-    if(meals==null){
-        let msg="No meal found.";
-        displayAlert(msg,"#f04086");
-    }
-    else{
-        meals.forEach((item) => {
-            let col=document.createElement("div");
-            col.setAttribute("class","col-3");
-            col.setAttribute("onclick","singleDisplay(event)");
-            let listItem=`
+	fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`).then((response) => {
+		return response.json();
+	}).then((mealData) => {
+		const meals = mealData.meals;
+
+		if (meals == null) {
+			let msg = "No meal found.";
+			displayAlert(msg, "#f04086");
+		} else {
+			meals.forEach((item) => {
+				let col = document.createElement("div");
+				col.setAttribute("class", "col-3");
+				col.setAttribute("onclick", "singleDisplay(event)");
+				let listItem = `
                <div class="card border cards shadow" style="curson:pointer">
                   <img src="${item.strMealThumb}" class="card-img-top" alt="...">
                   <div class="card-body">
@@ -49,73 +47,72 @@ const findMeals = (searchValue) => {
                   </div>
                </div>
              `;
-            col.innerHTML=listItem;
-            mealList.appendChild(col);
-        });
-    }
-})
+				col.innerHTML = listItem;
+				mealList.appendChild(col);
+			});
+		}
+	});
 }
 
 ////////////////////////////////
 //displayAlert function
 ///////////////////////////////
 
-const displayAlert = (message,color) => {
-    let alert=document.getElementById("alert");
-    alert.children[0].innerText=message;
-    alert.style.display="block";
-    alert.style.backgroundColor=color;
+const displayAlert = (message, color) => {
+	let alert = document.getElementById("alert");
+	alert.children[0].innerText = message;
+	alert.style.display = "block";
+	alert.style.backgroundColor = color;
 }
 
 ///////////////////////////
 //singleDisplay function
 ///////////////////////////
 const singleDisplay = (event) => {
-    let singleMain=event.currentTarget.children[0].children;
-    let mealName=singleMain[1].children[0].innerText;
-    mealName=mealName.trim();
-    singleDisplayItem(mealName);
+	let singleMain = event.currentTarget.children[0].children;
+	let mealName = singleMain[1].children[0].innerText;
+	mealName = mealName.trim();
+	singleDisplayItem(mealName);
 }
 
 //////////////////////////////////
 //singleDisplayItem function
 /////////////////////////////////
 const singleDisplayItem = (name) => {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`).then((res) => {
-        return res.json();
-    }).then((nameApi) => {
-        const item=nameApi.meals[0];
-        console.log(item);
-        let i=1;
-        document.getElementById("single-food-section").style.display="block";
-        const listGroup=document.getElementById("list-group");
-        listGroup.innerHTML="";
-        const singleItemImg=document.getElementById("single-item-img");
-        singleItemImg.setAttribute("src",`${item.strMealThumb}`);
-        document.getElementById('single-item-title').innerText=item.strMeal;
+	fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`).then((res) => {
+		return res.json();
+	}).then((nameApi) => {
+		const item = nameApi.meals[0];
+		console.log(item);
+		let i = 1;
+		document.getElementById("single-food-section").style.display = "block";
+		const listGroup = document.getElementById("list-group");
+		listGroup.innerHTML = "";
+		const singleItemImg = document.getElementById("single-item-img");
+		singleItemImg.setAttribute("src", `${item.strMealThumb}`);
+		document.getElementById('single-item-title').innerText = item.strMeal;
 
-        while(i!=0){
-            let ingredient=`strIngredient${i}`;
-            if(item[ingredient]===""||item[ingredient]===null){
-               break;
-            }
-            else{
-                //let listItem=`<li class="list-group-item">${item[ingredient]}</li>`;
-                let listItem=document.createElement("li");
-                listItem.setAttribute("class","list-group-item");
-                listItem.innerHTML =`<i class="fa fa-check-square text-success"></i> ${item[ingredient]}`;
-                listGroup.appendChild(listItem);
-                i++;
-            }
-            i++;
-        }
-            
-    })
+		while (i !== 0) {
+			let ingredient = `strIngredient${i}`;
+			if (item[ingredient] === "" || item[ingredient] === null) {
+				break;
+			} else {
+				//let listItem=`<li class="list-group-item">${item[ingredient]}</li>`;
+				let listItem = document.createElement("li");
+				listItem.setAttribute("class", "list-group-item");
+				listItem.innerHTML = `<i class="fa fa-check-square text-success"></i> ${item[ingredient]}`;
+				listGroup.appendChild(listItem);
+				i++;
+			}
+			i++;
+		}
+
+	})
 }
 
 ///////////////////////////////////
 //closeDisplay function
 ///////////////////////////////////
 const closeDisplay = (event) => {
-    document.getElementById("single-food-section").style.display="none";
+	document.getElementById("single-food-section").style.display = "none";
 }
